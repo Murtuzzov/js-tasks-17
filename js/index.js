9c88c773ea6ef1c7d814de018ce0563fa53f0835
Murtuzzov <131525655+Murtuzzov@users.noreply.github.com>
2024-03-24 22:53:31 +0400

    test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-//В ПРОЦЕССЕ
+//ЗАДАНИЕ No1
+
+// class Circle {
+//   radius;
+//   constructor(radius) {
+//     this.radius = radius;
+//   }
+
+//   getRadius() {
+//     return `Радиус окружности = ${this.radius}`;
+//   }
+
+//   setRadius(selectedRadius) {
+//     this.radius = selectedRadius;
+//   }
+
+//   getDiametr() {
+//     return `Диаметр окружности = ${this.radius * 2}`;
+//   }
+
+//   calculateArea() {
+//     return `Площадь окружности = ${Math.round(
+//       Math.PI * Math.pow(this.radius, 2)
+//     )}`;
+//   }
+
+//   calculateLength() {
+//     return ` Длина окружности = ${Math.round(this.radius * (2 * Math.PI))}`;
+//   }
+// }
+
+// const radiusCircle = new Circle(16);
+// console.log(radiusCircle.getRadius());
+// radiusCircle.setRadius(20);
+// console.log(radiusCircle.getRadius());
+// console.log(radiusCircle.getDiametr());
+// console.log(radiusCircle.calculateArea());
+// console.log(radiusCircle.calculateLength());
+
